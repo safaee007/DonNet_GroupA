bd6a5335c1c7236ade3ef667239aab06a765bbf5
S.Safaee <sokoot_ravi@yahoo.com>
2021-11-17 12:33:29 +0330

    class vs struct

---
+++ MyPersonStruct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practice
+{
+    public struct MyPersonStruct
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPerson p = new MyPerson();
+            p.FirstName = "saeed";
+
+            MyPerson p2 = p;
+            p2.FirstName = "ali";
+
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p2.FirstName);
+
+
+            //----------------------
+            MyPersonStruct ps = new MyPersonStruct();
+            ps.FirstName = "saeed";
+
+            MyPersonStruct ps2 = ps;
+            ps2.FirstName = "ali";
+
+            Console.WriteLine(ps.FirstName);
+            Console.WriteLine(ps2.FirstName);
+
+
+        }
+    }
+}
